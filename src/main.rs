2749e57d7fb4c4726aadd31d89daf9c29b78225f
riko-teki <boiledshrimps0109@gmail.com>
2022-03-25 22:55:19 +0900

    add error

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,47 @@
+use std::{fmt, error::Error};
+
 struct QR {
     version: u8,
     pixel_size: u8,
-}
-
-enum ErrCorrectionLevel {
-    L,
-    M,
-    Q,
-    H,
+    err_correction_level: ErrCorrectionLevel,
 }
 
 impl QR {
-    fn new(version: u8) -> Self {
+    fn new(version: u8) -> Result<Self, InvaridVersionError> {
         match version {
-            v @ 2_u8..=40_u8 => QR { 
+            v @ 2_u8..=40_u8 => Ok(QR { 
                 version: v,
-                pixel_size: version * 4 + 17
-            },
-            _ => unreachable!(),
+                pixel_size: version * 4 + 17,
+                err_correction_level: ErrCorrectionLevel::L,
+            }),
+            _ => Err(InvaridVersionError),
         } 
     }
 }
 
+enum ErrCorrectionLevel {
+    L = 7, // default
+    M = 15,
+    Q = 25,
+    H = 30,
+}
+
+#[derive(Debug, Clone)]
+struct InvaridVersionError;
+
+impl fmt::Display for InvaridVersionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid qr version was specified. Only numbers from 2 to 40 are valid.")
+    }
+}
+
+impl Error for InvaridVersionError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
 fn main() {
-    let qr = QR::new(40);
+    let qr = QR::new(40).unwrap();
     println!("{}",qr.pixel_size);
 }
